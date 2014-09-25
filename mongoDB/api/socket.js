3825c622234a1d4704c001ed224a981bf7d19641
tomplays <homeof@gmail.com>
2014-09-25 14:40:08 +0200

    deploy sync test

--- mongoDB/api/socket.js
+++ mongoDB/api/socket.js
@@ -4,8 +4,8 @@ var says;
 
 
 exports.socketer = function(socket, data){
-	console.log('socketz')
-	console.log(data)
-	socket.emit('newsback', data)
+	console.log('socket data')
+//	console.log(data)
+	//socket.emit('newsback', data)
 	socket.broadcast.emit('newsback', data)
 }
